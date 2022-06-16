4ae90f4303b4a249dc06b8e490511b8a8622e99b
Usama Mirza <usamak.mirza@gmail.com>
2022-06-16 20:39:12 +0500

    asd

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class ApiController < ApplicationController::API
+    class ApiController < ApplicationController
       include ActionController::HttpAuthentication::Token
       before_action :set_user
 
